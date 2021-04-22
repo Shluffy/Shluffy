7c364d3c3bc03965d9d681514a2cc97f11c98c20
dogsmakemehappy <81823039+Shluffy@users.noreply.github.com>
2021-04-22 16:33:06 -0400

    Remove unessacery uses of function

--- AutoBumpScratchTopic.js
+++ AutoBumpScratchTopic.js
@@ -1,9 +1,8 @@
 //This will auto bump a topic on the scratch discussion forums (assuming you can post in it)
-//Copy "javascript:function hello() { let t = document.getElementsByClassName("markup"); t[0].value = "Bump"; let b = document.getElementsByClassName("button grey"); b[2].click(); } hello()" to use
+//Copy "javascript:let t = document.getElementsByClassName("markup"); t[0].value = "Bump"; let b = document.getElementsByClassName("button grey"); b[2].click();" to use
 
-javascript:function hello() { 
+javascript:
   let t = document.getElementsByClassName("markup"); 
   t[0].value = "Bump"; 
   let b = document.getElementsByClassName("button grey"); 
-  b[2].click(); } 
-  hello()
+  b[2].click();
