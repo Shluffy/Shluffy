d19accfc2cd424826c64b6fff05c9fb812b6ba8e
dogsmakemehappy <81823039+Shluffy@users.noreply.github.com>
2021-04-25 15:26:11 -0400

    Delete RedirectToScratchUser.js

--- RedirectToScratchUser.js
+++
@@ -1,7 +0,0 @@
-//This bookmarklet will redirect you to a scratch user.
-//Copy "javascript:var user = prompt("What User Do You want to go to?"); if (user == "null") {window.location.href="https://scratch.mit.edu/users/"+user;};" to use.
-javascript:var user = prompt("What User Do You want to go to?"); 
-if (user == "null") 
-  {
-  window.location.href="https://scratch.mit.edu/users/"+user;
-  };
