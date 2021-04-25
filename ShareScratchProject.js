adb1a3478c97884c2508289da507e141a1e6b7eb
dogsmakemehappy <81823039+Shluffy@users.noreply.github.com>
2021-04-25 15:26:19 -0400

    Delete ShareScratchProject.js

--- ShareScratchProject.js
+++
@@ -1,4 +0,0 @@
-//This only works on the main project page, not in the editor.
-//Copy "javascript:let a = document.getElementsByClassName("banner-button"); a[0].click();" to use
-javascript:let a = document.getElementsByClassName("banner-button");
-  a[0].click()
