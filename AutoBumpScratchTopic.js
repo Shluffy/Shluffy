944596623c445bce3af0d03669e60e9399236369
dogsmakemehappy <81823039+Shluffy@users.noreply.github.com>
2021-04-25 15:25:40 -0400

    Delete AutoBumpScratchTopic.js

--- AutoBumpScratchTopic.js
+++
@@ -1,8 +0,0 @@
-//This will auto bump a topic on the scratch discussion forums (assuming you can post in it)
-//Copy "javascript:let t = document.getElementsByClassName("markup"); t[0].value = "Bump"; let b = document.getElementsByClassName("button grey"); b[2].click();" to use
-
-javascript:
-  let t = document.getElementsByClassName("markup"); 
-  t[0].value = "Bump"; 
-  let b = document.getElementsByClassName("button grey"); 
-  b[2].click();
