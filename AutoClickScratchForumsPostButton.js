6511dee89afb1bd4e20a9f8209070c44f5278c8a
dogsmakemehappy <81823039+Shluffy@users.noreply.github.com>
2021-04-25 15:25:46 -0400

    Delete AutoClickScratchForumsPostButton.js

--- AutoClickScratchForumsPostButton.js
+++
@@ -1,5 +0,0 @@
-//this will auto click the post button in the scratch discussion forums.
-//Copy "javascript:let b = document.getElementsByClassName("button grey"); b[2].click();" to use.
-
-javascript:let b = document.getElementsByClassName("button grey"); 
-b[2].click();
